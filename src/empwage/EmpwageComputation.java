e950cc1c52304af0cadf35001821ce2bc1254f41
Javul Mulla <javulmulla8@gmail.com>
2022-08-04 15:07:07 +0530

    javul: added attendence checker to the program

--- src/empwage/EmpwageComputation.java
+++ src/empwage/EmpwageComputation.java
@@ -1,7 +1,24 @@
 package empwage;
 
-public class EmpwageComputation {
-public static void main(String[] args) {
-	System.out.println("Welcome employee wage computation");
-}
-}
+
+	import java.util.Random;
+
+	public class EmpwageComputation {
+		public static void main(String[] args) {
+		
+			System.out.println("Welcome to Employee Wage computation.");
+		
+		 Random rand = new Random();
+
+	     int present = 1;
+	     int absent = 2;
+	     int check=rand.nextInt(3)+1;
+
+	     if (check==present) {
+	         System.out.println("Employee is present.");
+	     }
+	     else {
+	         System.out.println("Employee is absent.");
+	     }
+	}
+	}
